c60e171443b6eed3b645cd61fb90088e31c8815c
Jeffrey Lee <jglee1@msn.com>
2020-02-10 08:33:45 -0800

    Feature: First project

---
+++ Project1/project1.cpp
@@ -0,0 +1,26 @@
+//Jeffrey Lee
+//MyBlinkpart3
+//This project turns an LED on for one second, then off for one second, repeatedly.
+
+// the setup function runs once when you press reset or power the board
+#include <ArduinoSTL.h>
+using namespace std;
+
+int onTime;  // time led is on
+int offTime; // time led is off
+
+void setup() {
+  Serial.begin(9600);
+  cout << "What is your desired delay time in milliseconds?" << endl;
+  cin >> onTime;
+  pinMode(7, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(7, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(onTime);                       // wait for onTime period
+  digitalWrite(7, LOW);    // turn the LED off by making the voltage LOW
+  delay(offTime);         // wait for offTime period
+  offTime = onTime * 2;   // off time is double on time.
+}
